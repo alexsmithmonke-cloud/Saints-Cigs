35615256826e4f41cd5892cb9fa45e6a9d130de7
SinningSaint <alexsmithmonke@gmail.com>
2026-06-01 23:59:03 +0300

    Update sv_saintcigs.lua

--- saintcigs/lua/autorun/server/sv_saintcigs.lua
+++ saintcigs/lua/autorun/server/sv_saintcigs.lua
@@ -24,24 +24,54 @@ hook.Add("PlayerIsOverDosing", "Saints Overdose", function(ply)
     ply:ChatPrint("My breathing feels so clear... so clear!")
     ply:SetRunSpeed(ply:GetRunSpeed() * 1.5)
 
-    local id = ply:EntIndex()
-    local nameForOverdoseEffects = id.."ForOverdoseEffects"
-    timer.Create(nameForOverdoseEffects, 20, 1, function()
-        ply:SetRunSpeed(ply:GetRunSpeed() / 1.5)
+    if ply.ShotsTaken == nil then
+        ply.ShotsTaken = 1
+    else
+        ply.ShotsTaken = ply.ShotsTaken + 1
+    end
+    if ply.ShotsTaken >= 3 then
+        ply:Kill()
+        return 
+    end
+    local id = ply:EntIndex() 
+    local id2 = CurTime()
+    local nameForOverdoseEffects = id.."ForOverdoseEffects"..id2
+    local nameToCheckIfOverdose = id.."ToCheckIfOverdose"..id2
+    local amountOfOverdoesDmg = {20, 50, 100}
+    local shotstaken = ply.ShotsTaken
+    local effectlength = 5
+    timer.Create(nameForOverdoseEffects, 1, effectlength, function()
+        if ply:Alive() then
+            local dmg = (amountOfOverdoesDmg[shotstaken] / ply:GetMaxHealth()) * 100
+            local dmgtotake = dmg / effectlength
+            ply:ViewPunch(Angle(-1,0,0))
+            ply:SetHealth(ply:Health() - dmgtotake)
+            if ply:Health() < 1 then
+                timer.Remove(nameToCheckIfOverdose)
+                timer.Remove(nameForOverdoseEffects)
+                ply:Kill()
+            end
+        else
+            timer.Remove(nameToCheckIfOverdose)
+            timer.Remove(nameForOverdoseEffects)
+        end
     end)
-    timer.Create(nameForOverdoseEffects, 1, 20, function()
-        ply:ViewPunch(Angle(-10,0,0))
-        ply:SetHealth(ply:Health() - 2)
+    timer.Create(nameToCheckIfOverdose, effectlength, 1, function()
+        if ply:Alive() then
+            print("Fuck you")
+            ply:SetRunSpeed(ply:GetRunSpeed() / 1.5)
+        else
+            timer.Remove(nameToCheckIfOverdose)
+        end
     end)
-
-
-
 end)
 
 hook.Add("PlayerDeath", "ToResetValues", function(ply)
     local id = ply:EntIndex()
     local nameForAsthmaEffects = id.."ForAsthmaEffects"
     local nameForOverdoseEffects = id.."ForOverdoseEffects"
+    local nameToCheckIfOverdose = id.."ToCheckIfOverdose"
+    timer.Remove(nameToCheckIfOverdose)
     timer.Remove(nameForOverdoseEffects)
     timer.Remove(nameForAsthmaEffects)
     ply.HasAsthma = false
